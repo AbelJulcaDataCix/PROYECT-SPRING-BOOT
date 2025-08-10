8097fd7a00496e20b572cc49bb351d8997ba2917
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-08-10 00:38:55 -0500

    ABEL: 002 add parameters eureka in properties

---
+++ microservice-new/productService/src/main/java/com/dataprogramming/productService/model/Product.java
@@ -0,0 +1,26 @@
+package com.dataprogramming.productService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(value = "product")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
