9325f2d60cf623fc2f313f3db89fd0ec371c6946
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-08-18 21:43:09 -0500

    ABEL: 007 add change in the proyect

--- microservice-new/productService/src/main/java/com/dataprogramming/productService/ProductServiceApplication.java
+++ microservice-new/productService/src/main/java/com/dataprogramming/productService/ProductServiceApplication.java
@@ -1,13 +1,13 @@
-package com.dataprogramming.orderService;
+package com.dataprogramming.productService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
---
+++ microservice-new/productService/src/main/java/com/dataprogramming/productService/dto/ProductResponse.java
@@ -0,0 +1,21 @@
+package com.dataprogramming.productService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
