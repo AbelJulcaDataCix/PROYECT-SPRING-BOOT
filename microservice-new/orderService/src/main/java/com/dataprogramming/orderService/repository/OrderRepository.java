af0c8afe411857f91d524d1afae933f75aed52a3
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-08-18 21:35:51 -0500

    ABEL: 007 update change in proyect

--- microservice-new/orderService/src/main/java/com/dataprogramming/orderService/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dataprogramming.orderService.repository;
-
-
-import com.dataprogramming.orderService.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservice-new/productService/src/main/java/com/dataprogramming/productService/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dataprogramming.productService.repository;
-
-import com.dataprogramming.productService.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
