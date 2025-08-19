65f80141c0e704a31c04032c1694ec199a266240
AbelJulcaDataCix <datacixcustomer@gmail.com>
2025-08-18 21:36:51 -0500

    ABEL: 007 add change

---
+++ microservice-new/productService/src/main/java/com/dataprogramming/productService/dto/ProductRequest.java
@@ -0,0 +1,20 @@
+package com.dataprogramming.productService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
